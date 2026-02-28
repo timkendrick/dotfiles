47955a79e5b93c8fbbcd1f67847c91ab116c8033
Tim Kendrick <timkendrick@gmail.com>
2026-02-28 12:30:00 +0000

    Add `jj-watch` command

--- zsh-plugins/jj.zsh
+++ zsh-plugins/jj.zsh
@@ -1,1 +1,3 @@
 source <(jj util completion zsh)
+
+alias jj-watch='(repo_dir=$(jj root) && watchexec --quiet --clear --restart --watch "$repo_dir/.jj/repo/op_heads/heads" --ignore-nothing --wrap-process=none -- jj --ignore-working-copy log --color=always)'
