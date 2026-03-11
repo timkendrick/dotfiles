b07b69c8d9cd68c615e7a0f6d98ab9194978959a
Tim Kendrick <tkendrick@nanookadvisors.com>
2026-03-11 11:15:17 +0000

    Add `mise-link-global-npm-packages` command

--- zsh-plugins/mise.zsh
+++ zsh-plugins/mise.zsh
@@ -1,1 +1,5 @@
 source <(mise completion zsh)
+
+mise-link-global-npm-packages() {
+  for pkg in $(mise ls --json | jq -r 'keys[]' | grep '^npm:'); do (cd $(mise where $pkg)/lib/node_modules/$(<<< "$pkg" sed 's/^npm://') && npm link); done
+}
