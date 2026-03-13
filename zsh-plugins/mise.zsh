7567b4394053cdd1a879212cd65a06e74f6057c0
Tim Kendrick <timkendrick@gmail.com>
2026-03-13 09:41:21 +0000

    Allow globally linking individual mise `npm:` packages

--- zsh-plugins/mise.zsh
+++ zsh-plugins/mise.zsh
@@ -1,5 +1,10 @@
 source <(mise completion zsh)
 
 mise-link-global-npm-packages() {
-  for pkg in $(mise ls --json | jq -r 'keys[]' | grep '^npm:'); do (cd $(mise where $pkg)/lib/node_modules/$(<<< "$pkg" sed 's/^npm://') && npm link); done
+  # Read list of packages from arguments, defaulting to reading from mise directory if no arguments are provided
+  local pkgs=("$@")
+  if [ ${#pkgs[@]} -eq 0 ]; then
+    pkgs=($(mise ls --json | jq -r 'keys[]' | grep '^npm:'))
+  fi
+  for pkg in "${pkgs[@]}"; do (cd $(mise where $pkg)/lib/node_modules/$(<<< "$pkg" sed 's/^npm://') && npm link); done
 }
