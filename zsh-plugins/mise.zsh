54b11f8e1dcf72a94ae4a88b9ed9edda3c755ab7
Tim Kendrick <tkendrick@nanookadvisors.com>
2026-03-26 12:35:01 +0000

    Update `mise-link-global-npm-packages` to avoid `npm`

--- zsh-plugins/mise.zsh
+++ zsh-plugins/mise.zsh
@@ -6,5 +6,20 @@ mise-link-global-npm-packages() {
   if [ ${#pkgs[@]} -eq 0 ]; then
     pkgs=($(mise ls --json | jq -r 'keys[]' | grep '^npm:'))
   fi
-  for pkg in "${pkgs[@]}"; do (cd $(mise where $pkg)/lib/node_modules/$(<<< "$pkg" sed 's/^npm://') && npm link); done
+  for pkg in "${pkgs[@]}"; do
+    local pkg_dir="$(mise where $pkg)"
+    local pkg_name="$(<<< "$pkg" sed 's/^npm://')"
+    # Library packages need to be linked into the global node_modules directory
+    local lib_path="$(mise where node)/lib/node_modules/$pkg_name"
+    if [ -d "$lib_path" ]; then
+      echo "Linked: $pkg"
+      continue
+    else
+      echo "Linking $pkg..."
+      local pkg_path="$pkg_dir/lib/node_modules/$pkg_name"
+      mkdir -p "$(dirname "$lib_path")"
+      ln -s "$pkg_path" "$lib_path"
+      echo "Linked: $lib_path > $pkg_path"
+    fi
+  done
 }
