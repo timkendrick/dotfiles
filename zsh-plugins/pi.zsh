1b3db3074231ea2ac152bf335d6c9d729ed5fb73
Tim Kendrick <timkendrick@gmail.com>
2026-04-16 10:08:13 +0100

    Bypass `pi` IDE diff editor

--- zsh-plugins/pi.zsh
+++ zsh-plugins/pi.zsh
@@ -1,1 +1,3 @@
+export PI_DE_CLAUDE_USE_DIFF_EDITOR=false
+
 alias 'pi-sandbox'="sandbox --dir . $(jj_root=$(jj workspace root 2>/dev/null) && { echo --dir $jj_root; if [ -f $jj_root/.jj/repo ]; then echo --dir $(dirname $(dirname $(cat $jj_root/.jj/repo))); fi; } | tr "\n" " " || true) --dir ~/.pi --dir ~/.npm --dir ~/Library/pnpm --dir ~/.docker/buildx --dir ~/.plannotator -- pi"
