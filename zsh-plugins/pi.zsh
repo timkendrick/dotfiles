c8e0523f97463bfde947fb62e09ba0af3db3e31a
Tim Kendrick <timkendrick@gmail.com>
2026-04-16 10:08:13 +0100

    Add `pi` system prompt

--- zsh-plugins/pi.zsh
+++ zsh-plugins/pi.zsh
@@ -12,7 +12,7 @@ alias 'pi-sandbox'="sandbox \
   $(git_root=$(git rev-parse --show-cdup 2>/dev/null) && echo --dir "$(cd "$git_root" && pwd)" || true) \
   $(jj_root=$(jj workspace root 2>/dev/null) && { echo --dir $jj_root; if [ -f $jj_root/.jj/repo ]; then echo --dir $(dirname $(dirname $(cat $jj_root/.jj/repo))); fi; } | tr "\n" " " || true) \
   -- \
-  pi"
+  pi --tools read,bash,edit,write,grep,find,ls"
 
 pi-install() {
   local pkg="$1"
