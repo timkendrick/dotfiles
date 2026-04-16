67982b090c46298bf354834141fc12fb4b76923b
Tim Kendrick <timkendrick@gmail.com>
2026-04-16 10:08:11 +0100

    Standardize `$PACKAGE_ROOT` usage across plugins

--- zsh-plugins/mise.zsh
+++ zsh-plugins/mise.zsh
@@ -1,6 +1,6 @@
-# Get the directory of the current script
-SCRIPT_PATH="${0:a:h}"
+# Get the grandparent directory of the current script
+PACKAGE_ROOT=$(realpath "${0:a:h}/..")
 
-export MISE_GLOBAL_CONFIG_FILE="$(realpath "$SCRIPT_PATH/../config/mise.toml")"
+export MISE_GLOBAL_CONFIG_FILE="$PACKAGE_ROOT/config/mise.toml"
 
 eval "$(mise activate zsh)"
