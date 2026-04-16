fb72f9623a95a768193056a7729762979d669ad3
Tim Kendrick <timkendrick@gmail.com>
2026-04-16 10:08:12 +0100

    Set `vim` as default zsh `VISUAL` editor

---
+++ zsh-plugins/vim.zsh
@@ -0,0 +1,6 @@
+export VISUAL="vim"
+
+# Assigning VISUAL to `vim` causes zsh to use vim keybindings for shell command input,
+# so we disable the vim keybindings in the shell by activating 'emacs' mode.
+bindkey -e
+
