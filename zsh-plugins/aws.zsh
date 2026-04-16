02094c0eacc571c83fc41aa521a820f44ece48ad
Tim Kendrick <timkendrick@gmail.com>
2026-04-16 10:08:11 +0100

    Update `aws-env` documentation

--- zsh-plugins/aws.zsh
+++ zsh-plugins/aws.zsh
@@ -2,6 +2,7 @@
 alias aws-session='aws sso login && eval $(aws configure export-credentials --format env)'
 
 # Expose AWS environment variables to the prefixed command
+# Use with AWS_PROFILE=... environment variable to invoke the command with the specified profile
 # Usage: aws-env <command> # AWS_ACCESS_KEY_ID=... AWS_SECRET_ACCESS_KEY=... AWS_SESSION_TOKEN=... <command>
 # N.B. Implemented as a function rather than an alias in order to preserve completions for the prefixed command
 aws-env() {
