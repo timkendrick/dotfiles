408e771f584b385cff6df4cc0b64ab5199a17eec
Tim Kendrick <timkendrick@gmail.com>
2026-04-16 10:08:11 +0100

    Ensure `mise` plugin is activated before other plugins

--- zsh-plugins/mise.zsh
+++ zsh-plugins/mise.zsh
@@ -1,8 +1,1 @@
-# Get the grandparent directory of the current script
-PACKAGE_ROOT=$(realpath "${0:a:h}/..")
-
-# Use the mise config file from this repository
-export MISE_GLOBAL_CONFIG_FILE="$PACKAGE_ROOT/config/mise.toml"
-
-# Activate mise
-eval "$(mise activate zsh)"
+source <(mise completion zsh)
