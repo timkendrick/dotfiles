9ac7649daebc1e8fc87d9a815cb15c36bda14ec2
Tim Kendrick <timkendrick@gmail.com>
2026-04-16 10:08:11 +0100

    Ensure local `node_modules` take priority over global mise `node_modules`

--- zsh-plugins/mise.zsh
+++ zsh-plugins/mise.zsh
@@ -4,3 +4,6 @@ PACKAGE_ROOT=$(realpath "${0:a:h}/..")
 export MISE_GLOBAL_CONFIG_FILE="$PACKAGE_ROOT/config/mise.toml"
 
 eval "$(mise activate zsh)"
+
+# Ensure local node_modules are in the PATH before global node_modules
+PATH="./node_modules/.bin:$PATH"
