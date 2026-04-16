746455da3eb3980580bcf971bcb1291746902ab1
Tim Kendrick <timkendrick@gmail.com>
2026-04-16 10:08:11 +0100

    Add AWS auth commands

---
+++ zsh-plugins/aws.zsh
@@ -0,0 +1,11 @@
+# Login to AWS account via SSO and set environment variables
+alias aws-session='aws sso login && eval $(aws configure export-credentials --format env)'
+
+# Expose AWS environment variables to the prefixed command
+# Usage: aws-env <command> # AWS_ACCESS_KEY_ID=... AWS_SECRET_ACCESS_KEY=... AWS_SESSION_TOKEN=... <command>
+# N.B. Implemented as a function rather than an alias in order to preserve completions for the prefixed command
+aws-env() {
+    env $(aws configure export-credentials --format env-no-export | xargs) "$@"
+}
+# For zsh completions, treat the first argument as a command
+compdef '_command aws-env' aws-env
