c86b833b974256150aac0798397ff4756ded82f7
Tim Kendrick <timkendrick@gmail.com>
2026-09-08 15:56:24 +0100

    Add `tt-context-add` / `tt-worktree-delete-current` / `tt-pi` aliases

--- zsh-plugins/tt.zsh
+++ zsh-plugins/tt.zsh
@@ -2,3 +2,6 @@ export TT_SELECT=fzf
 export TT_VERBOSE=1
 
 alias tt-worktree-select='cd "$(tt worktree show --name "$(tt worktree list --quiet | fzf)" || echo ".")"'
+alias tt-worktree-delete-current='tt worktree delete --force "$(tt worktree show --name "$(tt current)")" && exit'
+alias tt-context-add='(file="$(ls .agents/plans/*.md | fzf)" && tt task context add --title "$(edit)" < "$file" && rm "$file")'
+alias tt-pi='pi "$(tt prompt --message "$(edit)")"'
